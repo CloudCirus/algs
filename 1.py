18cf85925d6b1277db24dac37b7b2484a51f3cde
Cloud <smirnovceo@yandex.ru>
2021-12-16 17:49:22 +0500

    Forgot add 1.py task and refactoring

---
+++ 1.py
@@ -0,0 +1,13 @@
+from typing import Union
+
+
+def fined_elem(arg: str, target: str) -> Union[str, int]:
+    for char in arg:
+        if target == char:
+            return char
+        else:
+            return -1
+
+
+if __name__ == '__main__':
+    print(fined_elem('Алгоритмика', 'А'))
